bb59ec4329d9d2aaad65761eb53d1a77ac1e8ac7
artprange <arthur.prange@gmail.com>
2024-04-10 18:01:49 -0300

    swiper bug

--- main.js
+++ main.js
@@ -1,6 +1,33 @@
+
+// carrossel
+const swiper = new Swiper(".mySwiper", {
+  direction: 'vertical',
+  loop: true,
+  effect: 'cards',
+  grabCursor: true,
+
+  cardEffect: {
+      slideShadows: false,
+      cardShadows: false,
+      rotate: false,
+      grabCursor: true,
+  },
+
+  autoplay: {
+      delay: 5000,
+  },
+});
+// fim carrossel
+
+
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+
+
+
