fa942243245863c804227efe67987c217e92d933
AAndersonSantos <andersonsantos.inc01@gmail.com>
2024-04-10 23:41:35 -0300

    Ajustes Carosel com OWL

--- main.js
+++ main.js
@@ -1,25 +1,21 @@
-
-// carrossel
-const swiper = new Swiper(".mySwiper", {
-  direction: 'vertical',
-  loop: true,
-  effect: 'cards',
-  grabCursor: true,
-
-  cardEffect: {
-      slideShadows: false,
-      cardShadows: false,
-      rotate: false,
-      grabCursor: true,
-  },
-
-  autoplay: {
-      delay: 5000,
-  },
-});
-// fim carrossel
-
-
+$('#carousel-1').owlCarousel({
+  loop:true,
+  margin: 40,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
 
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
