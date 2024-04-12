e20c870ec794e0ff024cf7909850583f596130bb
artprange <arthur.prange@gmail.com>
2024-04-12 13:10:10 -0300

    viddeo acelerado, blur no fundo

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+
+
+var video = document.getElementById("myVideo");
+
+    // Set playback speed to 1.5x
+    video.playbackRate = 2.0;
