f47f5b1e0d24774d10cfeb3dcb4e779af659765c
Anderson Santos <anderson.santos@fngroup.com.br>
2024-04-12 12:26:39 -0300

    Add Container-p4

--- main.js
+++ main.js
@@ -39,6 +39,28 @@ $('#carousel-2').owlCarousel({
   }
 })
 
+$('#carousel-3').owlCarousel({
+  loop:true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  nav:true,
+  responsive:{
+      0:{
+        items:1
+      },
+      400:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+})
+
 $('#carousel-4').owlCarousel({
   loop:true,
   margin: 10,
