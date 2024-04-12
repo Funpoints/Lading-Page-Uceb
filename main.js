e32e8a1c704777d846749206d768efba160ec75d
Anderson Santos <anderson.santos@fngroup.com.br>
2024-04-12 15:25:43 -0300

    Add containerpt-6

--- main.js
+++ main.js
@@ -83,6 +83,77 @@ $('#carousel-4').owlCarousel({
   }
 })
 
+$('#carousel-5').owlCarousel({
+  loop:true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  nav: false,
+  dots: false,
+  center: true,
+  responsive:{
+      0:{
+        items:2
+      },
+      400:{
+          items:3
+      },
+      600:{
+          items:5
+      },
+      1000:{
+          items:6
+      }
+  }
+})
+
+$('#carousel-6').owlCarousel({
+  loop:true,
+  margin: 1,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  nav: true,
+  dots: true,
+  responsive:{
+      0:{
+        items:1
+      },
+      400:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
+$('#carousel-7').owlCarousel({
+  loop:true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  nav: false,
+  dots: false,
+  center: true,
+  responsive:{
+      0:{
+        items:2
+      },
+      400:{
+          items:3
+      },
+      600:{
+          items:5
+      },
+      1000:{
+          items:6
+      }
+  }
+})
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
