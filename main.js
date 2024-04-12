f8b66047457851cf9b39f90eaa33da875403c44a
artprange <arthur.prange@gmail.com>
2024-04-12 14:33:47 -0300

    depoimentos

--- main.js
+++ main.js
@@ -83,6 +83,30 @@ $('#carousel-4').owlCarousel({
   }
 })
 
+
+
+$('#carousel-6').owlCarousel({
+  loop:true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 4000,
+  nav:true,
+  responsive:{
+      0:{
+        items:1
+      },
+      400:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:3
+      }
+  }
+})
+
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
