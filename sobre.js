a88a23a2da8aa07b76562dd393517e8feb888188
artprange <arthur.prange@gmail.com>
2024-04-16 11:30:03 -0300

    p4 feito

--- sobre.js
+++ sobre.js
@@ -96,3 +96,31 @@ jQuery(function ($) {
 	$this.countTo(options);
   }
 });
+
+
+// carrossel
+
+
+$('#carousel-1').owlCarousel({
+	loop:true,
+	margin: 100,
+	autoplay: true,
+	autoplayTimeout: 1000,
+	nav: false,
+	dots: false,
+	center: true,
+	responsive:{
+		0:{
+		  items:2
+		},
+		400:{
+			items:3
+		},
+		600:{
+			items:5
+		},
+		1000:{
+			items:6
+		}
+	}
+  })
